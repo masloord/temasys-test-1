bdc2b7bfcffc58162ce36f689ff63a83f885bee6
masloord <masloord@gmail.com>
2017-10-03 15:41:15 +0800

    work

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const path = require('path')
 // ...
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
-app.get('/*', function (req, res) {
+app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'))
 })
 app.use(forceSSL())
