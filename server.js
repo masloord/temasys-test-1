46b5cf0fdb9b7d67c50786e0d3b3689b8dcd5464
masloord <masloord@gmail.com>
2017-10-03 16:16:29 +0800

    pls work heroku

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 const express = require('express')
 const app = express()
 const http = require('http')
+const path = require('path')
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'))
+app.use(express.static(path.join(__dirname + 'dist')))
 // Start the app by listening on the default
 // Heroku port
 // Instruct the app
 // to use the forceSSL
 // middleware
 
-const path = require('path')
 // ...
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
-app.get('/*', function (req, res) {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/dist/index.html'))
 })
 
