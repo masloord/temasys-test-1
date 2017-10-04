607dae28f2ea49fa157b39d30404f49e8c541587
clityaw <clityaw@gmail.com>
2017-10-04 17:19:20 +0800

    testing new init method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -93,12 +93,17 @@ this.sl.joinRoom("testxx", {
     audio: true,
     video: true
   }, function (error, success) {
-    if (error) return;
-    console.log("User connected with getUserMedia() Stream.")
-    this.sl.getPeers(true, function (error, success) {
-       if (error) return;
-       console.log("The list of all Peers in the same App space ->", success);
-    });
+    if (error) {
+        document.getElementById('status').innerHTML = 'Failed retrieval for room information.<br>Error: ' + (error.error.message || error.error);
+      } else {
+           document.getElementById('status').innerHTML = 'Room information has been loaded. Room is ready for user to join.';
+        document.getElementById('start').style.display = 'block';
+        console.log("User connected with getUserMedia() Stream.")
+        this.sl.getPeers(true, function (error, success) {
+           if (error) return;
+           console.log("The list of all Peers in the same App space ->", success);
+        });
+      }
   });
 }
 
