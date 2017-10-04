0fb13db6f947d523294d52c226393f6f36d1f9d5
clityaw <clityaw@gmail.com>
2017-10-04 18:06:24 +0800

    testing new init method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -34,7 +34,7 @@ this.sl.on('peerJoined', function(peerId, peerInfo, isSelf) {
   if(isSelf) return; // We already have a video element for our video and don't need to create a new one.
   var vid = document.createElement('video');
   vid.autoplay = true;
-  vid.muted = true; // Added to avoid feedback when testing locally
+  vid.muted = false; // Added to avoid feedback when testing locally
   vid.id = peerId;
   document.body.appendChild(vid);
 });
